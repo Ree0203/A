2bbc1aab27cf1daa1dd77cd8d8e1977eb1f49852
Ree0203 <alternative.acc00000@gmail.com>
2025-11-28 22:49:41 +0800

    added letter to main

--- mainscript.js
+++ mainscript.js
@@ -5,4 +5,31 @@ document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() { 
         greetCard.classList.add("fade-out"); 
     }, 22000); 
+
+    var envelope = document.getElementById('envelope');
+    
+    
+    envelope.addEventListener("click",  function() { 
+        if(envelope.classList.contains("open")){ 
+            envelope.classList.add("close");
+            envelope.classList.remove("open"); 
+        } else if(envelope.classList.contains("close")) { 
+            envelope.classList.add("open"); 
+            envelope.classList.remove("close"); 
+        } else { 
+            envelope.classList.add("open"); 
+        }
+    }); 
+
+
+   
+    
+    function open() {
+        envelope.classList.add("open")
+        envelope.classList.remove("close");
+    }
+    function close() {
+        envelope.classList.add("close")
+        envelope.classList.remove("open");
+    }
 }); 
